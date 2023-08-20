a14034c5dc4ce98792c9d3912ae8054adbb4500d
JeremiUnity <130007527+JFroggo-Gaming@users.noreply.github.com>
2023-08-20 22:00:11 +0100

    Inventory Manager Class
    - Item details are visible in the console when we walk on an item(pickup script)
    New Scripts:
    - ItemPickUp.cs
    - InventoryManager.cs

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class InventoryManager : SingletonMonobehaviour<InventoryManager>
+{
+    private Dictionary<int, ItemDetails> itemDetailsDictionary;  // Stores item code and corresponding item details
+    
+    [SerializeField] private SO_ItemList itemList = null; //  is a reference to a Scriptable Object called SO_ItemList, which holds a list of ItemDetails.
+
+    private void Start()
+    {
+        // Create item details dictionary
+        CreateItemDetailsDictionary();
+    }
+
+    // Populates the itemDetailsDictionary from the scriptable object items list
+    private void CreateItemDetailsDictionary()
+    {
+        itemDetailsDictionary = new Dictionary<int, ItemDetails>();
+
+        foreach (ItemDetails itemDetails in itemList.itemDetails)
+        {
+            itemDetailsDictionary.Add(itemDetails.itemCode, itemDetails);
+        }
+    }
+
+    // Returns the itemDetails (from the SO_ItemList) for the itemCode, or null if the item code doesn't exist
+    public ItemDetails GetItemDetails(int itemCode)
+    {
+        ItemDetails itemDetails;
+
+        if (itemDetailsDictionary.TryGetValue(itemCode, out itemDetails))
+        {
+            return itemDetails;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
--- Assets/Scripts/Item/SO_ItemList.cs
+++ Assets/Scripts/Item/SO_ItemList.cs
@@ -1,9 +1,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName="so_ItemList", menuName="Scriptable Objects/Item/ItemList")]
+// ScriptableObject to store a list of item details
+[CreateAssetMenu(fileName = "so_ItemList", menuName = "Scriptable Objects/Item/ItemList")]
 public class SO_ItemList : ScriptableObject
 {
     [SerializeField]
-    public List<ItemDetails> ItemDetails;
+    public List<ItemDetails> itemDetails; // List of item details contained in this ScriptableObject
 }
