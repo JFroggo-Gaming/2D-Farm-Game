530d6981cc6ddc9918cb71f42c975dd0aa0dbffc
Jeremi-Unity <jeremi.rosowski@gmail.com>
2023-10-03 20:39:03 +0200

    The animations are changed when a player carries an item 1/2
    When a player carries an item it is shown in the form of new animation
    
    New Scripts:
    - AnimationOverrides
    - CharacterAttribute
    - SO_AnimationType
    
    Updated scripts:
    - Enums

--- Assets/Scripts/Enums/Enums.cs
+++ Assets/Scripts/Enums/Enums.cs
@@ -1,3 +1,65 @@
+public enum AnimationName
+{
+    IdleDown,
+    IdleUp,
+    IdleRight,
+    IdleLeft,
+    walkUp,
+    walkDown,
+    walkRight,
+    walkLeft,
+    runUp,
+    runDown,
+    runLeft,
+    useToolUp,
+    useToolDown,
+    useToolRight,
+    useToolLeft,
+    swingToolUp,
+    swingToolDown,
+    swingToolRight,
+    swingToolLeft,
+    liftToolUp,
+    liftToolDown,
+    liftToolRight,
+    liftToolLeft,
+    holdToolUp,
+    holdToolDown,
+    holdToolRight,
+    holdToolLeft,
+    pickDown,
+    pickUp,
+    pickRight,
+    pickLeft,
+    count
+}
+
+public enum CharacterPartAnimator
+{
+    body,
+    arms,
+    hair,
+    tool,
+    hat,
+    count
+}
+public enum PartVariantColour
+{
+    none,
+    count
+}
+
+public enum PartVariantType
+{
+    none,
+    carry,
+    hoe,
+    pickaxe,
+    axe,
+    scythe,
+    wateringCan,
+    count
+}
 public enum InventoryLocation
 {
     player,
